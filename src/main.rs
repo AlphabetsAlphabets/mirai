7f0363c361dc5b83678d46b7df14bdacd98db79d
Yap Jia Hong <yapjiahong2003@gmail.com>
2022-04-11 14:40:28 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use eframe::{egui, epi};
+
+#[derive(Default)]
+struct App;
+
+impl epi::App for App {
+    fn name(&self) -> &str {
+        "app"
+    }
+
+    fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
+        egui::Window::new("Test").show(ctx, |ui| {
+            ui.label("Hello, world!");
+        });
+    }
+}
+
+fn ui_counter(ui: &mut egui::Ui, counter: &mut i32) {
+    ui.horizontal(|ui| {
+        if ui.button("-").clicked() {
+            *counter -= 1;
+        }
+        ui.label(counter.to_string());
+        if ui.button("+").clicked() {
+            *counter += 1;
+        }
+    });
+}
+
+fn main() {
+    let app = App::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
+}
